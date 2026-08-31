dba731f69ae4bf758431bda94a5c9124fe5b24bf
isaiahnoelpulidosalazar <isaiahnoelpulidosalazar@gmail.com>
2026-08-31 16:11:33 +0800

    remodel Actions class

--- Actions.cs
+++ Actions.cs
@@ -7,7 +7,7 @@ namespace inpsNuGet;
 public class Actions
 {
     readonly Action Action;
-    public Task RunningTask { get; private set; }
+    public Task? RunningTask { get; private set; }
     public bool IsRunning => RunningTask != null && !RunningTask.IsCompleted;
 
     public Actions(Action Action)
@@ -21,13 +21,70 @@ public class Actions
         return this;
     }
 
+    public Actions RunExe(string FilePath, params string[] FileArguments)
+    {
+        RunningTask = ExeRunner(FilePath, FileArguments);
+        return this;
+    }
+
+    public async Task ExeRunner(string FilePath, params string[] FileArguments)
+    {
+        ProcessStartInfo ProcessStartInfo = new ProcessStartInfo
+        {
+            FileName = FilePath,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        foreach (string Argument in FileArguments)
+        {
+            ProcessStartInfo.ArgumentList.Add(Argument);
+        }
+
+        using Process Process = new Process { StartInfo = ProcessStartInfo };
+        Process.Start();
+
+        Task<string> OutputTask = Process.StandardOutput.ReadToEndAsync();
+        Task<string> ErrorTask = Process.StandardError.ReadToEndAsync();
+
+        await Process.WaitForExitAsync();
+
+        string Output = await OutputTask;
+        string Error = await ErrorTask;
+
+        Console.WriteLine($"Exit Code: {Process.ExitCode}");
+        Console.WriteLine($"Output: {Output}");
+        if (!string.IsNullOrEmpty(Error))
+        {
+            Console.WriteLine($"Error: {Error}");
+        }
+    }
+
     public Actions RunOnDedicatedThread(bool DoInBackground = true)
     {
-        var Thread = new Thread(new ThreadStart(Action))
+        TaskCompletionSource TaskCompletionSource = new TaskCompletionSource();
+
+        var Thread = new Thread(() =>
+        {
+            try
+            {
+                Action();
+                TaskCompletionSource.SetResult();
+            }
+            catch (Exception e)
+            {
+                TaskCompletionSource.SetException(e);
+            }
+        })
         {
             IsBackground = DoInBackground
         };
+
+        RunningTask = TaskCompletionSource.Task;
         Thread.Start();
+
         return this;
     }
 }
--- Changes.cs
+++ Changes.cs
@@ -3,7 +3,7 @@ namespace inpsNuGet;
 public class Changes
 {
     // changes:
-    // - rework SimpleFileHandler.ProjectToLocation functions
+    // - remodel Actions class
 
     public Changes()
     {
